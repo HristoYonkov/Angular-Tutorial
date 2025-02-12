174bea96047e9b7cbc273994ca557814cceab853
Hristo Yonkov <hristopturs@gmail.com>
2025-02-12 17:53:20 +0200

    Update cart.service.ts

--- joes-robo-shop/src/app/cart.service.ts
+++ joes-robo-shop/src/app/cart.service.ts
@@ -14,3 +14,34 @@ export class CartService {
     console.log(`Product: "${product.name}" added to cart.`);
   }
 }
+
+// in future we may use following code
+// private cart: ILineItem[] = [];
+
+//   getTotalPrice() {
+//     return (
+//       Math.round(
+//         this.cart.reduce<number>((prev, cur) => {
+//           return (
+//             prev + cur.qty * (cur.product.price * (1 - cur.product.discount))
+//           );
+//         }, 0) * 100
+//       )
+//     )
+//   }
+
+//   findLineItem() {
+//     return this.cart.find((li) => li.product.id === product.id);
+//   }
+
+//   add(product: IProduct) {
+//     let lineItem = this.findLineItem(product);
+//     if (lineItem !== undefined) {
+//       lineItem.qty ++;
+//     } else {
+//       lineitem = {product: product, qty: 1};
+//       this.cart.push(lineItem);
+//     }
+//     console.log(`Product: "${product.name}" added to cart.`);
+//     console.log('Total Price: + ' this.getTotalPrice());
+//   }
