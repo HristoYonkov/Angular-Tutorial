515d3027e8a3ad970789871a29aa1b665b8f43e9
Hristo Yonkov <hristopturs@gmail.com>
2025-02-13 14:19:53 +0200

    Update catalog.component.ts

--- joes-robo-shop/src/app/catalog/catalog.component.ts
+++ joes-robo-shop/src/app/catalog/catalog.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
 import { ProductDetailsComponent } from '../product-details/product-details.component';
-import { CartService } from '../cart.service';
+import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
 
 // We can add directly styles into the decorator but usually we want this for
