375a59861bd6a6f7d4eb514e1ff0000e4b42e80d
Hristo Yonkov <hristopturs@gmail.com>
2025-02-13 14:19:44 +0200

    Update app.component.ts

--- joes-robo-shop/src/app/app.component.ts
+++ joes-robo-shop/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
+import { CartComponent } from './cart/cart.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { SiteHeaderComponent } from './site-header/site-header.component';
     RouterOutlet,
     HomeComponent,
     CatalogComponent,
-    SiteHeaderComponent
+    SiteHeaderComponent,
+    CartComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
