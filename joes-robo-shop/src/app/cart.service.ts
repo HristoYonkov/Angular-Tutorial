64fb903341ba358cc3cf4ade88a2ca40171805d3
Hristo Yonkov <hristopturs@gmail.com>
2025-02-13 13:46:39 +0200

    Update cart.service.ts

--- joes-robo-shop/src/app/cart.service.ts
+++ joes-robo-shop/src/app/cart.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  constructor() { }
+  private cart: IProduct[] = [];
 
-  cart: IProduct[] = [];
+  constructor(private http: HttpClient) { }
 
   add(product: IProduct) {
     this.cart.push(product);
-    console.log(`Product: "${product.name}" added to cart.`);
+    this.http.post('api/cart', this.cart).subscribe(() => {
+      console.log(`Product: "${product.name}" added to cart.`);
+    });
   }
 }
 
