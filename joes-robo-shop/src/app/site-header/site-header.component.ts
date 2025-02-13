4ad624210be869b5d58595be1e857076d0bf366b
Hristo Yonkov <hristopturs@gmail.com>
2025-02-13 15:38:42 +0200

    Update site-header.component.ts

--- joes-robo-shop/src/app/site-header/site-header.component.ts
+++ joes-robo-shop/src/app/site-header/site-header.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'bot-site-header',
+  imports: [RouterLink],
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
