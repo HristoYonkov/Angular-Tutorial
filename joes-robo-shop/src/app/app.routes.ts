e6c5dc97f1dd50842536e26a6576ce84ba84cdad
Hristo Yonkov <hristopturs@gmail.com>
2025-02-14 10:00:50 +0200

    Update app.routes.ts

--- joes-robo-shop/src/app/app.routes.ts
+++ joes-robo-shop/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartComponent } from './cart/cart.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent, title: 'home - Joe\'s robot shop'},
-    { path: 'catalog', component: CatalogComponent, title: 'catalog - Joe\'s robot shop' },
+    { path: 'catalog', component: CatalogComponent },
     { path: 'cart', component: CartComponent, title: 'cart - Joe\'s robot shop' },
+    { path: 'cart', component: CartComponent, title: 'cart - Joe\'s robot shop' },
+    { path: 'sign-in', component: SignInComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home
     { path: '**', redirectTo: '' } // Wildcard route for 404
 ];
