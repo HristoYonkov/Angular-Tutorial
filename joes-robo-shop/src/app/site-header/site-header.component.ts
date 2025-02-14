fc5b592225d59394f882a9496b9db78acaedbe95
Hristo Yonkov <hristopturs@gmail.com>
2025-02-14 13:58:16 +0200

    Update site-header.component.ts

--- joes-robo-shop/src/app/site-header/site-header.component.ts
+++ joes-robo-shop/src/app/site-header/site-header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'bot-site-header',
@@ -8,7 +10,23 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrls: ['./site-header.component.css']
 })
 export class SiteHeaderComponent {
+  user: IUser | null = null;
+  isSignOut: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
+  ngOnInit() {
+    this.userService.getUser().subscribe({
+      next: (user) => {this.user = user}
+    })
+  }
+
+  toggleSignOut() {
+    this.isSignOut = !this.isSignOut;
+  }
+
+  signOut() {
+    this.userService.signOut();
+    this.isSignOut = false;
+  }
 }
