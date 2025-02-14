0d3698387e22d50d14b25190e1a432eac201c0ed
Hristo Yonkov <hristopturs@gmail.com>
2025-02-14 09:10:33 +0200

    use empty path for redirecting to home

--- joes-robo-shop/src/app/app.routes.ts
+++ joes-robo-shop/src/app/app.routes.ts
@@ -7,5 +7,6 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent, title: 'home - Joe\'s robot shop'},
     { path: 'catalog', component: CatalogComponent, title: 'catalog - Joe\'s robot shop' },
     { path: 'cart', component: CartComponent, title: 'cart - Joe\'s robot shop' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to home
     { path: '**', redirectTo: '' } // Wildcard route for 404
 ];
