97c7948e305eea4b0696fddb816bd1ef5b9407df
Hristo Yonkov <hristopturs@gmail.com>
2025-02-14 12:31:44 +0200

    Update sign-in.component.ts

--- joes-robo-shop/src/app/user/sign-in/sign-in.component.ts
+++ joes-robo-shop/src/app/user/sign-in/sign-in.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { IUserCredentials } from '../user.model';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bot-sign-in',
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent {
-
-  constructor() { }
-
+  credentials: IUserCredentials = { email: '', password: '' }
+  
+  constructor(private userService: UserService, private router: Router) { }
+  
+  signIn() {
+    this.userService.signIn(this.credentials).subscribe( {
+      next: () => this.router.navigate(['/catalog'])
+    })
+  }
 }
