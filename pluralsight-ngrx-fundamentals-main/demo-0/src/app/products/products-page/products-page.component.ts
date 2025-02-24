d164aff1eebcf0defb848444fa7e0ff93dc5a235
Hristo Yonkov <hristopturs@gmail.com>
2025-02-24 16:14:19 +0200

    Update products-page.component.ts

--- pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/products-page/products-page.component.ts
+++ pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/products-page/products-page.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Store } from '@ngrx/store';
-import { ProductsAPIActions, ProductsPageActions } from '../state/products.actions';
-import { selectProducts, selectProductsLoading, selectProductsTotal } from '../state/products.selectors';
+import { ProductsPageActions } from '../state/products.actions';
+import { selectProducts, selectProductsLoading, selectProductsShowProductCode, selectProductsTotal } from '../state/products.selectors';
 
 @Component({
   selector: 'app-products-page',
@@ -13,31 +13,16 @@ export class ProductsPageComponent {
   products$ = this.store.select(selectProducts);
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading);
-  showProductCode$ = this.store.select(
-    (state: any) => state.products.showProductCode
-  );
+  showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage = '';
 
-  constructor(
-    private productsService: ProductsService,
-    private store: Store
-  ) {
+  constructor(private store: Store) {
     this.store.subscribe((store) => console.log(store)
     )
   }
 
   ngOnInit() {
-    this.getProducts();
-  }
-
-  getProducts() {
-    this.store.dispatch(ProductsPageActions.loadProducts())
-    this.productsService.getAll().subscribe({
-      next: (products) => {
-        this.store.dispatch(ProductsAPIActions.productsLoadedSuccess({ products }))
-      },
-      error: (error) => (this.errorMessage = error),
-    });
+    this.store.dispatch(ProductsPageActions.loadProducts());
   }
 
   toggleShowProductCode() {
