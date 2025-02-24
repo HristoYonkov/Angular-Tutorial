0602fb77256b4c924ec4be34737af3278f512b12
Hristo Yonkov <hristopturs@gmail.com>
2025-02-24 10:21:49 +0200

    Update products.reducer.ts

--- pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/state/products.reducer.ts
+++ pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/state/products.reducer.ts
@@ -9,6 +9,7 @@ const initialState: ProductsState = {
     showProductCode: true
 }
 
+// Dispatching process, phase 3. Actual actions are happend here.
 export const productsReducer = createReducer(
     initialState,
     on(ProductsPageActions.toggleShowProductCode, (state) => ({
