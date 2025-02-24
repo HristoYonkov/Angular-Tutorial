c38648e1697bf8a62178389c79ec858182546459
Hristo Yonkov <hristopturs@gmail.com>
2025-02-24 16:14:21 +0200

    Create products.effects.ts

---
+++ pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/state/products.effects.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { ProductsService } from "../products.service";
+import { ProductsAPIActions, ProductsPageActions } from "./products.actions";
+import { concatMap, map } from "rxjs";
+
+@Injectable()
+
+export class ProductEffects {
+
+    constructor(
+        private actions$: Actions,
+        private productService: ProductsService
+    ) { }
+
+    loadProducts$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(ProductsPageActions.loadProducts),
+            concatMap(() =>
+                this.productService.getAll().pipe(
+                    map((products) => ProductsAPIActions.productsLoadedSuccess({ products }))
+                ),
+            )
+        )
+    )
+}
