b1dc915cf19c7b506fa3fc86b80377eaeb4249bf
Hristo Yonkov <hristopturs@gmail.com>
2025-02-25 14:29:21 +0200

    Update products.reducer.ts

--- pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/state/products.reducer.ts
+++ pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/state/products.reducer.ts
@@ -16,24 +16,27 @@ const initialState: ProductsState = {
     errorMessage: ''
 }
 
-// Dispatching process, phase 3. Actual actions are happend here.
 export const productsReducer = createReducer(
     initialState,
     on(ProductsPageActions.toggleShowProductCode, (state) => ({
         ...state,
         showProductCode: !state.showProductCode,
     })),
+    // Dispatching process, phase 3. Actual actions are happend here.
     on(ProductsPageActions.loadProducts, (state) => ({
         ...state,
         loading: true,
         products: [],
         errorMessage: '',
     })),
-    on(ProductsAPIActions.productsLoadedSuccess, (state, { products }) => ({
-        ...state,
-        loading: false,
-        products,
-    })),
+    on(ProductsAPIActions.productsLoadedSuccess, (state, { products }) => {
+        console.log("Log from reducers.");
+        return ({
+            ...state,
+            loading: false,
+            products,
+        })
+    }),
     on(ProductsAPIActions.productsLoadedFail, (state, { message }) => ({
         ...state,
         products: [],
