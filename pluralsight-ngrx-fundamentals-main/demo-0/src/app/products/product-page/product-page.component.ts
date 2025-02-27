18640079b7bddd56eeb5e95f500136f12cec6d45
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-02-27 18:39:28 +0200

    Update product-page.component.ts

--- pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/product-page/product-page.component.ts
+++ pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/product-page/product-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Product } from '../product.model';
-import { ProductsService } from '../products.service';
 import { Store } from '@ngrx/store';
 import { selectProductById } from '../state/products.selectors';
+import { ProductsPageActions } from '../state/products.actions';
 
 @Component({
   selector: 'app-product-page',
@@ -14,22 +14,20 @@ export class ProductPageComponent {
   product$ = this.store.select(selectProductById);
 
   constructor(
-    private productsService: ProductsService,
     private router: Router,
-    private activatedRoute: ActivatedRoute,
     private store: Store
-  ) {}
+  ) { }
 
   addProduct(product: Product) {
-    this.productsService.add(product).subscribe(this.goToProductsPage);
+    this.store.dispatch(ProductsPageActions.addProduct({ product }))
   }
 
   updateProduct(product: Product) {
-    this.productsService.update(product).subscribe(this.goToProductsPage);
+    this.store.dispatch(ProductsPageActions.updateProduct({ product }))
   }
 
   deleteProduct(id: number) {
-    this.productsService.delete(id).subscribe(this.goToProductsPage);
+    this.store.dispatch(ProductsPageActions.deleteProduct({ id }))
   }
 
   goToProductsPage = () => this.router.navigate(['/products']);
