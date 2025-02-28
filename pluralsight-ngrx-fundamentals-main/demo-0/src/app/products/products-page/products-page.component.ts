0458022532f7a832455e50b4f57755e210d38fe6
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-02-28 17:10:08 +0200

    Update products-page.component.ts

--- pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/products-page/products-page.component.ts
+++ pluralsight-ngrx-fundamentals-main/demo-0/src/app/products/products-page/products-page.component.ts
@@ -8,21 +8,34 @@ import {
   selectProductsShowProductCode,
   selectProductsTotal
 } from '../state/products.selectors';
+import { ProductsStore } from '../product.store';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  providers: [ProductsStore]
 })
 export class ProductsPageComponent {
-  products$ = this.store.select(selectProducts);
+  products$ = this.productsStore.products$;
+  // products$ = this.store.select(selectProducts);
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading);
   showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage$ = this.store.select(selectProductsErrorMessage);
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store,
+    private productsStore: ProductsStore
+  ) {
     this.store.subscribe((store) => console.log(store));
+  }
+
+  // Following ngOnInit() is for component-store pattern.
+  // If we want to use 'NgRx redux store pattern' we must comment
+  // "ngOnInit() and "products$ = this.productsStore.products$;"
+  ngOnInit() {
+    this.productsStore.getProducts();
   }
 
   // ngOnInit() {
