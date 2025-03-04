08c61e6b802b99020cf4c4389d9e1cf92aca4c61
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-03-04 12:41:28 +0200

    Update edit-contact.component.ts

--- angular-reactive-forms/contacts-app/src/app/edit-contact/edit-contact.component.ts
+++ angular-reactive-forms/contacts-app/src/app/edit-contact/edit-contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
@@ -17,14 +17,30 @@ export class EditContactComponent implements OnInit {
 
   // Following code is how to do it with FormGroup() way.
   contactForm = new FormGroup({
+    id: new FormControl(),
     firstName: new FormControl(),
     lastName: new FormControl(),
     dateOfBirth: new FormControl(),
     favoritesRanking: new FormControl(),
+    phone: new FormGroup({
+      phoneNumber: new FormControl(),
+      phoneType: new FormControl(),
+    }),
+    address: new FormGroup({
+      streetAddress: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      postalCode: new FormControl(),
+      addressType: new FormControl(),
+    })
   });
 
 
-  constructor(private route: ActivatedRoute, private contactsService: ContactsService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private contactsService: ContactsService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
@@ -39,10 +55,20 @@ export class EditContactComponent implements OnInit {
       // this.dateOfBirth.setValue(contact.dateOfBirth);
       // this.favoritesRanking.setValue(contact.favoritesRanking);
 
+      this.contactForm.controls.id.setValue(contact.id);
       this.contactForm.controls.firstName.setValue(contact.firstName);
       this.contactForm.controls.lastName.setValue(contact.lastName);
       this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
       this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking);
+      // Here is how we will make nested FormGroup for phone in our contactForm.
+      this.contactForm.controls.phone.controls.phoneNumber.setValue(contact.phone.phoneNumber);
+      this.contactForm.controls.phone.controls.phoneType.setValue(contact.phone.phoneType);
+      // Here is how we will make nested FormGroup for address in our contactForm.
+      this.contactForm.controls.address.controls.streetAddress.setValue(contact.address.streetAddress);
+      this.contactForm.controls.address.controls.city.setValue(contact.address.city);
+      this.contactForm.controls.address.controls.state.setValue(contact.address.state);
+      this.contactForm.controls.address.controls.postalCode.setValue(contact.address.postalCode);
+      this.contactForm.controls.address.controls.addressType.setValue(contact.address.addressType);
     })
   }
 
@@ -53,6 +79,11 @@ export class EditContactComponent implements OnInit {
     // Following code is how to do it with FormGroup() way.
     // console.log(this.contactForm.controls.firstName.value);
     console.log(this.contactForm.value);
-    
+    // getRawValue() will always return all controlled form property
+    // values, even if the control is disabled.
+    // this.contactsService.saveContact(this.contactForm.value);
+    this.contactsService.saveContact(this.contactForm.getRawValue()).subscribe({
+      next: () => this.router.navigate(['/contacts'])
+    });
   }
 }
