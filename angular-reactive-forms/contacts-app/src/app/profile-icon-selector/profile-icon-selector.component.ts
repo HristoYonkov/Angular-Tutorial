06e39eb8dd9a82b9170cfb1fb4da58fe66ecf060
Hristo Yonkov <50380455+HristoYonkov@users.noreply.github.com>
2025-03-06 13:09:56 +0200

    Update profile-icon-selector.component.ts

--- angular-reactive-forms/contacts-app/src/app/profile-icon-selector/profile-icon-selector.component.ts
+++ angular-reactive-forms/contacts-app/src/app/profile-icon-selector/profile-icon-selector.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Provider } from '@angular/core';
 import { profileIconNames } from './profile-icon-names';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const DATE_VALUE_PROVIDER: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => ProfileIconSelectorComponent),
+  multi: true,
+}
 
 @Component({
   selector: 'con-profile-icon-selector',
